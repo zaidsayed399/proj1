64d5c0d7fa309f0562ae5de0fbee3065c8ed18fc
zaidsayed399 <zaidsayed399@gmail.com>
2022-02-17 15:41:56 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png">
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <home />
+  <contact />
+
+
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import home from './components/home.vue'
+import contact from './components/contactus.vue'
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld,
+    home,
+    contact,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/contactus.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="contact"> 
+<h1>Contact Us Component</h1>
+    <div class="mb-3">
+  <label  class="form-label">Email address</label>
+  <input type="email" class="form-control"  placeholder="name@example.com">
+</div><br><br>
+<div class="mb-3">
+  <label  class="form-label">Example textarea</label>
+  <textarea class="form-control"  rows="3"></textarea>
+</div>
+<br><br>
+<button type="submit">Submit</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'contact',
+}
+</script>
+<style scoped>
+.contact{
+
+}
+</style>
+
